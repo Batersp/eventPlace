8717982e577c6d62e0ed64c0249936297f41a203
pavel_ishmukov <p-pup1@mail.ru>
2022-10-02 22:52:40 +0300

    menu burger done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,25 +8,27 @@ export const Header = () => {
                 <a href='' className='header__logo'>
                     <img src={logo} alt="logo"/>
                 </a>
-                <nav className='header__menu menu'>
-                    <ul className='menu__list'>
-                        <li className='menu__item'>
-                            <a href="" className='menu__link'>HOME</a>
-                        </li>
-                        <li className='menu__item'>
-                            <a href="" className='menu__link'>FEATURES</a>
-                        </li>
-                        <li className='menu__item'>
-                            <a href="" className='menu__link'>SUPPORT</a>
-                        </li>
-                        <li className='menu__item'>
-                            <a href="" className='menu__link'>CONTACT US</a>
-                        </li>
-                    </ul>
-
-                </nav>
+                <div className='header__menu menu'>
+                    <nav id='menu' className='menu__body'>
+                        <ul className='menu__list'>
+                            <li className='menu__item'>
+                                <a href="" className='menu__link'>HOME</a>
+                            </li>
+                            <li className='menu__item'>
+                                <a href="" className='menu__link'>FEATURES</a>
+                            </li>
+                            <li className='menu__item'>
+                                <a href="" className='menu__link'>SUPPORT</a>
+                            </li>
+                            <li className='menu__item'>
+                                <a href="" className='menu__link'>CONTACT US</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
                 <div className='header__button'>
                     <a href="" className='button button_blue'>DOWNLOAD</a>
+                    <a href='#menu' className='icon-menu'><span></span></a>
                 </div>
             </div>
 
