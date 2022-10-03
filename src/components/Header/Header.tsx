07b54c061bed2f560ec8fc9279b7781bf4fc858e
pavel_ishmukov <p-pup1@mail.ru>
2022-10-03 09:15:47 +0300

    update burger

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,13 +3,13 @@ import logo from '../../assets/image/Group 2.svg'
 
 export const Header = () => {
     return (
-        <div className='header'>
-            <div className={'header__container contentContainer'}>
+        <div id='open' className='header'>
+            <div id='close' className={'header__container contentContainer'}>
                 <a href='' className='header__logo'>
                     <img src={logo} alt="logo"/>
                 </a>
                 <div className='header__menu menu'>
-                    <nav id='menu' className='menu__body'>
+                    <nav className='menu__body'>
                         <ul className='menu__list'>
                             <li className='menu__item'>
                                 <a href="" className='menu__link'>HOME</a>
@@ -28,7 +28,8 @@ export const Header = () => {
                 </div>
                 <div className='header__button'>
                     <a href="" className='button button_blue'>DOWNLOAD</a>
-                    <a href='#menu' className='icon-menu'><span></span></a>
+                    <a href='#open' className='icon-menu'><span></span></a>
+                    <a href='#close' className='close-icon-menu'></a>
                 </div>
             </div>
 
